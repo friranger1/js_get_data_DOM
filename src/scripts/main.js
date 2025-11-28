4bb70a666d4607343c11b1c7f934db502920cb66
friranger <65720979+friranger1@users.noreply.github.com>
2025-11-28 23:04:25 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const elem = [...document.querySelectorAll('span.population')];
+const totalElem = document.querySelector('span.total-population');
+const averageElem = document.querySelector('span.average-population');
+let total = 0;
+let average = 0;
+
+for (const e of elem) {
+  const value = e.innerHTML.replaceAll(',', '');
+  total += +value;
+}
+
+average = Math.round(total / elem.length);
+
+totalElem.innerText = total.toLocaleString('en-US');
+averageElem.innerHTML = average.toLocaleString('en-US');
